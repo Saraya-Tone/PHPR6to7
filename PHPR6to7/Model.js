3e9e332c891c0351ac8a5f70c67cb8496366db97
kunidevteam <kunitone.hub@gmail.com>
2013-07-30 15:20:48 +0900

    Initial commit

---
+++ PHPR6to7/Model.js
@@ -0,0 +1,27 @@
+﻿
+guidedModel =// @startlock
+{
+	ListHTML :
+	{
+		methods :
+		{// @endlock
+			loadTestdata:function()
+			{// @lock
+				var testLines = loadText( ds.getModelFolder().path + "testdata/TT10M24AA2TTv7.html" );
+				return testLines;
+			},// @lock
+			convertHTML:function(lines)
+			{// @lock
+				var v6HTML = lines.split("\n");
+				var len = v6HTML.length;
+				var v7HTML = new Array(len);
+				var v7HTML = "";
+				for ( var i=0; i<len; i++) {
+					v7HTML= v7HTML + v6HTML[i] + "\n";
+				}
+				
+				return v7HTML;
+			}// @startlock
+		}
+	}
+};// @endlock
---
+++ PHPR6to7/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,30 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+	var Convert = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		var testLines = ds.ListHTML.loadTestdata();
+		$$('v6Field').setValue(testLines);
+		return;
+	};// @lock
+
+	Convert.click = function Convert_click (event)// @startlock
+	{// @endlock
+		var v6lines  = $$('v6Field').getValue();
+		var v7lines = ds.ListHTML.convertHTML(v6lines);
+		$$('v7Field').setValue(v7lines);
+		return;
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+	WAF.addListener("Convert", "click", Convert.click, "WAF");
+// @endregion
+};// @endlock
